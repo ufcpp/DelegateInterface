46e035e6925bdca85ca2d16a861cc3964e7e3ffb
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-05-30 14:09:18 +0900

    lock

--- src/DelegateInterface/Cache.cs
+++ src/DelegateInterface/Cache.cs
@@ -9,15 +9,24 @@ public static class Cache
 {
     private static readonly DelegateInterfaceTypeBuilder _builder = new();
 
-    internal static Type Create(Type interfaceType) => _builder.Build(interfaceType);
+    internal static Type Create(Type interfaceType)
+    {
+        lock (_builder)
+        {
+            return _builder.Build(interfaceType);
+        }
+    }
 
     private readonly static Dictionary<Type, Type> _interfaceToProsy = new();
 
     public static Type GetProxyType(Type interfaceType)
     {
-        ref var proxyType = ref CollectionsMarshal.GetValueRefOrAddDefault(_interfaceToProsy, interfaceType, out var exists);
-        if (!exists) proxyType = Create(interfaceType);
-        return proxyType!;
+        lock (_interfaceToProsy)
+        {
+            ref var proxyType = ref CollectionsMarshal.GetValueRefOrAddDefault(_interfaceToProsy, interfaceType, out var exists);
+            if (!exists) proxyType = Create(interfaceType);
+            return proxyType!;
+        }
     }
 
     public static object CreateInstance(Type interfaceType) => Activator.CreateInstance(GetProxyType(interfaceType))!;
